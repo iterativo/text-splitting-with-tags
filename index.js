d8ad293e1fb779a956a5d9fa1fedaf2e4773432f
Rafael Torres <iterativo@users.noreply.github.com>
2020-03-25 20:29:34 -0500

    Rename l to chunkLen for readability

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// Split the provided text into chunks of length `l` with appended `(x/n)` tag
-// where:
+// Split the provided text into chunks of length `chunkLen` with appended `(x/n)` tag.
+//
+// Where:
 //      x = current chunk number 
 //      n = total number of chunks
 
 const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis accumsan quam, eget venenatis purus. Curabitur ac massa quis sem porta imperdiet vitae eget ipsum. Pellentesque dapibus lacus non viverra fermentum. Cras sodales metus vel quam dictum, ac luctus mauris sollicitudin. Aenean at dolor lobortis, condimentum sapien id, bibendum velit. Nunc in nunc erat. Integer ligula nisi, porttitor mattis venenatis vitae, facilisis vitae justo. Donec tristique sem vel diam tristique, eget venenatis enim faucibus. Fusce urna quam, bibendum vel turpis quis, porta eleifend diam. Cras arcu ex, sagittis ac hendrerit a, venenatis non sapien. Mauris vehicula semper ligula. Fusce vulputate tempus nisi, sit amet rutrum est commodo eu. Ut semper sodales sapien vel maximus. Nulla lacinia convallis ex, eu mollis nibh dapibus eu."
 
-const chunkify = (text, l, segCount) => {
+const chunkify = (text, chunkLen, segCount) => {
     const tagLength = getTagLength(segCount);
-    const segLength = l - tagLength + 1;
+    const segLength = chunkLen - tagLength + 1;
     const regex = '.{1,' + segLength + '}';
     const chunks = text.match(new RegExp(regex, 'g'));
     return chunks.map((c, i) => c + tag(i, segCount));
@@ -27,18 +28,18 @@ const countExtraChunkTagChars = (segCount) => {
     return extraChunkTagChars;
 }
 
-const getSegCount = (textLength, l) => {
-    let segCount = Math.ceil(text.length / l);
+const getSegCount = (textLength, chunkLen) => {
+    let segCount = Math.ceil(text.length / chunkLen);
     const extraChunkTagChars = countExtraChunkTagChars(segCount);
-    segCount = Math.ceil((text.length + extraChunkTagChars)/l);   
+    segCount = Math.ceil((text.length + extraChunkTagChars)/chunkLen);   
     return segCount;  
 }
 
-const solve = (text, l) => {
-    const segCount = getSegCount(text.length, l);
-    const chunks = chunkify(text, l, segCount);
-    console.log('chunks', chunks);
+const solve = (text, chunkLen) => {
+    const segCount = getSegCount(text.length, chunkLen);
+    const chunks = chunkify(text, chunkLen, segCount);
+    return chunks;
 }
 
-const l = 25;
-solve(text, l);
+const chunkLen = 25;
+console.log(solve(text, chunkLen));
